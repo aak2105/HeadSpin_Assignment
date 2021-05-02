d89cdd9994cef3e1bc15b13ceb3e4e60d0c0a1f1
anu.a <anu.a@zyxware.com>
2021-05-02 21:52:01 +0530

    sum of series pgm code added

--- series_sum.py
+++ series_sum.py
@@ -1,1 +1,38 @@
+"""
+Assignment Qstn 2
 
+Program to find the sum of the following series
+	Series: 1/1! + 2/2! +3/3! + …. + N/N!
+
+"""
+ 
+def seriesSum(number):
+  result = 0
+  factorial = 1
+
+  for i in range(1, num+1):
+    factorial = factorial * i
+    result = result + (i / factorial)
+  return result
+
+num = int(input('Enter the series limit: '))
+print("Sum of the series with limit ", num, "is", seriesSum(num))
+
+
+"""
+def sumOfSeries(num):
+     
+    res = 0
+    fact = 1
+     
+    for i in range(1, num+1):   
+        fact *= i
+        res = res + (i/ fact)
+
+         
+    return res
+
+n = int(input('Enter a number: '))     
+#n = 5
+print("Sum: ", sumOfSeries(n))
+"""
