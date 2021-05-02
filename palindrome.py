70a5837c29247f42b2de40810cbb637b2aaa6c9c
anu.a <anu.a@zyxware.com>
2021-05-02 18:52:55 +0530

    pgm for checking a number is palindrome or not is completed

--- palindrome.py
+++ palindrome.py
@@ -1,1 +1,26 @@
+"""
+Assignment Qstn 1
+Finds if a number is palindrome or not.
+
+"""
+
+num = int(input('Enter a number: '))
+num_copy = num
+reversed_num = 0
+
+#print(num,num_copy,reversed_num)
+
+#print("while loop starts")
+while(num > 0):
+  #print(num)
+  digit = num % 10
+  #print(digit)
+  reversed_num = (reversed_num * 10) + digit
+  #print(reversed_num)
+  num = num // 10
+
+if(num_copy == reversed_num):
+  print(str(num_copy) + " is a palindrome number")
+else:
+  print(str(num_copy) + " is not a palindrome number")
 
