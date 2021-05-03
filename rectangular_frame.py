f45e7ec9a33f1786144db376ec930a9c40f36920
anu.a <anu.a@zyxware.com>
2021-05-03 11:04:54 +0530

    redefined function with additional space before and after list word added as multiline comment

--- rectangular_frame.py
+++ rectangular_frame.py
@@ -7,6 +7,7 @@ prints them one per line in a rectangular frame
 
 """
 
+#"""
 def rectangular_frame(list_words):
   width = max(len(word) for word in list_words)
   print("*" * (width+2))
@@ -15,6 +16,22 @@ def rectangular_frame(list_words):
     print("*" + word + " " * ((width) - len(word)) + "*")
   
   print("*" * (width+2))
+#"""
+
+"""
+redefined function for adding a space before and after 
+each word in the list for better readability
+"""
+"""
+def rectangular_frame(list_words):
+  width = max(len(word) for word in list_words)
+  print("*" * (width+4)) # 2 extra * needed
+
+  for word in list_words:
+    print("*" + " " + word + " " * ((width+1) - len(word)) + "*") #additional space before and after word
+  
+  print("*" * (width+4)) # 2 extra * needed
+"""
 
 list1 = ["Hello","world","in","a","frame"]
 rectangular_frame(list1)
